3357000c03886ea117e392c6f4eb0944a6d3effb
shahvatsal712 <shahvatsal712@gmail.com>
2026-02-25 11:28:39 +0530

    Add files via upload
    Submission of Assignment 1

---
+++ Assignment_1(Task_1).py
@@ -0,0 +1,19 @@
+"""
+Write a python program that does the following:
+1. Takes two numbers as input from the user
+2. Performs the basic mathematical operations on these two numbers:
+    a. Addition
+    b. Subtraction
+    c. Multiplication
+    d. Division
+"""
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c= a+b
+d = a-b
+e = a*b
+f = a/b
+print("Addition: ", c)
+print("Subtraction: ", d)
+print("Multiplication: ", e)
+print("Division: ", f)
---
+++ Assignment_1(Task_2).py
@@ -0,0 +1,6 @@
+A= input("Enter your first name: ")
+B= input("Enter your last: ")
+C= A+" "+B
+print("Hello, ",C,"!","Welcome to Python program.")
+
+
